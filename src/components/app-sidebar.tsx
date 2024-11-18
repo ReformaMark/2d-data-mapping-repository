a0e7a750af0757a34c827bc838dd4625576f9d7d
DrrylPns <56997112+DrrylPns@users.noreply.github.com>
2024-11-19 01:35:28 +0800

    remove a1 logo

--- src/components/app-sidebar.tsx
+++ src/components/app-sidebar.tsx
@@ -206,9 +206,9 @@ export function AppSidebar({
       <SidebarHeader>
         <Link href={baseUrl} className="flex flex-col items-center mb-8">
           <div className="bg-[#8BC34A] w-full py-4 rounded-md flex flex-col items-center">
-            {value !== 'admin' && (
+            {/* {value !== 'admin' && (
               <Image src="/logo.svg" alt="A1 Agro" width={50} height={50} className="size-16" />
-            )}
+            )} */}
             <h1 className="text-sm font-semibold text-center text-white mt-2 px-2">
               {header}
             </h1>
